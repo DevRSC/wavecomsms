939ab9ed0f1c064fc185b491a70b91204ce18acf
RamilSC <ramil.cobillajr@tup.edu.ph>
2023-04-13 14:27:32 +0800

    API Version

--- sms-server.js
+++ sms-server.js
@@ -6,26 +6,25 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
 
 const serialPort = new SerialPort({
   path: "COM3",
   baudRate: 115200,
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/send-sms", async (req, res) => {
-  const phoneNumber = req.body.phoneNumber;
-  const message = req.body.message;
+app.get("/send-sms", async (req, res) => {
+  const phoneNumber = req.query.phoneNumber;
+  const message = req.query.message;
 
   try {
     await sendSMS(serialPort, phoneNumber, message);
-    res.status(200).send("Message sent successfully");
+    res
+      .status(200)
+      .json({ status: "success", message: "Message sent successfully" });
   } catch (error) {
-    res.status(500).send("Failed to send message");
+    res
+      .status(500)
+      .json({ status: "error", message: "Failed to send message" });
   }
 });
 
@@ -46,5 +45,5 @@ function sendSMS(port, phoneNumber, message) {
 }
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`SMS API is running on port ${port}`);
 });
